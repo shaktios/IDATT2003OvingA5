f71e6321181f9462b2031b5f0b84e286a4e0e389
Shakti Om Sharma <shaktiforgithub@gmail.com>
2025-03-14 01:08:08 +0100

    final push

--- src/main/java/no/ntnu/idatt2003/oving5/Main.java
+++ src/main/java/no/ntnu/idatt2003/oving5/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import no.ntnu.idatt2003.oving5.gui.GameGUI;
 
 public class Main {
-    //test
+
     public static void main(String[] args) {
         Application.launch(GameGUI.class, args);
     }
